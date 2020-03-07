4f1a4a77ec86f819b87c829dc63c9f3421e9246c
jenoki <jenoki@gill>
2020-03-07 22:22:01 +0900

    0.32
    First SSD1306 OLED implementation.

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -70,7 +70,7 @@ typedef struct {
 /* Exported macro ------------------------------------------------------------*/
 /* USER CODE BEGIN EM */
 #define USBD_DEVICE_VER_MAJ	0x00
-#define USBD_DEVICE_VER_MIN	0x31
+#define USBD_DEVICE_VER_MIN	0x32
 /* USER CODE END EM */
 
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
